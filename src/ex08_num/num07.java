41ad617757fde574c1160ccc51672ac4afe768f1
yujin9907 <heoyujin07@gmail.com>
2022-08-02 17:55:15 +0900

    0802

---
+++ src/ex08_num/num07.java
@@ -0,0 +1,20 @@
+package ex08_num;
+
+class Login{
+	private String id;
+	
+	void user(String s) {
+		if(id.equals(s)) {
+			System.out.println("맞음");
+		}
+	}
+}
+
+public class num07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
